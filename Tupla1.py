1125b5be134a38a22e73590727d9b1e7d0e5f8db
izadora-oliveira <74382460+izadora-oliveira@users.noreply.github.com>
2020-12-07 19:08:29 -0300

    Create Tupla1.py

---
+++ Tupla1.py
@@ -0,0 +1,9 @@
+# Uma lanchonete precisa urgente de um Cardápio com 5 itens,
+# crie um Cardápio com tupla e mostre o resultado no final.
+
+
+lanches = ('hamburguer','hotdog','pastel','coxinha','empadinha')
+precos = ('7,00','6,00','5,00','3,00','2,50')
+
+for i in range(0,len(lanches)) :
+    print(lanches[i],"...........", precos[i])
